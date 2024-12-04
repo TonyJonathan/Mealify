b17abbbc26f6b121eeb93fedb93c4fc7a070d62e
tony <tony.bruchon@hotmail.fr>
2024-12-05 00:48:31 +0100

    Added the input that will serve as a filter.

--- mealify/src/components/Banner.js
+++ mealify/src/components/Banner.js
@@ -1,5 +1,6 @@
 import "../styles/Banner.css"
 import logo from "../assets/logo.svg"
+import searchLogo from "../assets/searchLogo.png"
 
 function Banner() {
   return (
@@ -7,6 +8,10 @@ function Banner() {
       <h1 className="top-banner">Votre menu de la semaine en un clic !</h1>
       <div className="div-logo">
         <img src={logo} alt="mealify"></img>
+        <div className="div-search-input">
+          <img src={searchLogo} alt="search logo" width="30px" height="30px" className="search-logo"></img>
+          <input type="text" placeholder="Rechercher..." className="search-input"></input>
+        </div>
       </div>
     </>
   )
