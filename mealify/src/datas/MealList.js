a56c9a1fad877ac672565b2b7a5d655dce68c012
tony <tony.bruchon@hotmail.fr>
2024-12-05 23:23:29 +0100

    Added the 'datas' folder and the 'MealList.js' file containing the list of dishes and their various characteristics.

---
+++ mealify/src/datas/MealList.js
@@ -0,0 +1,34 @@
+export const MealList = [
+  {
+    name: "Boulettes alla bolognese", 
+    description: "Des boulettes bolognaise pour un remake de la Belle et le Clochard...",
+    nutriscore: "A", 
+    preparationTime: "10 minutes",
+    cookingTime: "15 minutes", 
+    kcal: "739", 
+    proteins: "40g",
+    carbohydrates: "89g", 
+    Lipids: "23g", 
+    ustensils: "Casserole, Poêle, Plaques de cuisson, Passoire.", 
+    seasoning: "Huile d'olive (1 càc)", 
+    ingredients: [
+      ["Pâtes (spaghetti)", 100, "g"],
+      ["Parmesan (râpé)", 1, "càs"],
+      ["Boeuf (steack haché frais)", 1],
+      ["Échalote", 0.25],
+      ["Sauce tomate", 80, "g"],
+      ["Basilic (frais)", 0.1, "bou."],
+      ["Chapelure", 1, "càs"],
+      ["Œuf", 0.25]
+    ], 
+    recipe: [
+      ["Si besoin, faites décongeler les steaks hachés. Émincez l'échalote finement.", ["Échalote"]],
+      ["Dans un récipient ajoutez le boeuf, l'oeuf, la chapelure, l'échalote, la moitié du parmesan/personne, salez, poivrez, malaxez le tout avec les mains.", ["Œuf", "Chapelure", "Parmesan (râpé)", "Boeuf (steack haché frais)"]], 
+      ["Faire des boulettes de 4 cm environ en les roulant entre vos doigts."], 
+      ["Cuire les pâtes selon les instructions du paquet dans une eau salée.", ["Pâtes (spaghetti)"]], 
+      ["Faire revenir les boulettes 5 minutes à la poêle bien chaude avec un filet d'huile d'olive."], 
+      ["En fin de cuisson, baisser le feu au minimum, ajoutez la sauce tomate, 1 cuillère à soupe d'eau/personne et laissez mijoter le temps de la prochaine étape.", "Sauce tomate"], 
+      ["Une fois les pâtes cuites, égouttez-les, puis mélangez-les avec la sauce et les boulettes. Servir avec un peu de parmesan (optionnel : et de basilic frais si vous en avez) sur le dessus. C'est prêt !", ["Basilic (frais)", "Parmesan (râpé)"]]
+    ]
+  }
+]
