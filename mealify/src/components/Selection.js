d96923e3416209877eb7e5dd7b2fda2d33ad55ef
tony <tony.bruchon@hotmail.fr>
2024-12-06 21:27:49 +0100

    Updated the layout of the banner and the selection list to adapt to media queries, functional down to 882px width.

--- mealify/src/components/Selection.js
+++ mealify/src/components/Selection.js
@@ -2,12 +2,15 @@ import "../styles/Selection.css"
 
 function Selection() {
   return (
+    <>
     <div className="selectionList">
       <div className="selection">VIANDES</div>
       <div className="selection">POISSON</div>
       <div className="selection">VÉGÉ</div>
       <div className="selection"> &lt;660KCAL </div>
     </div>
+    <div className="separationLine"></div>
+    </>
   )
 }
 
