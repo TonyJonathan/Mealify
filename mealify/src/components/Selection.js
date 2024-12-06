67c6dc237de939b658df89b50896521ad44993f4
tony <tony.bruchon@hotmail.fr>
2024-12-06 01:29:20 +0100

    Added the names of the different dish category options

--- mealify/src/components/Selection.js
+++ mealify/src/components/Selection.js
@@ -3,10 +3,10 @@ import "../styles/Selection.css"
 function Selection() {
   return (
     <div className="selectionList">
-      <div className="selection">Pour bientôt</div>
-      <div className="selection">Pour bientôt</div>
-      <div className="selection">Pour bientôt</div>
-      <div className="selection">Pour bientôt</div>
+      <div className="selection">VIANDES</div>
+      <div className="selection">POISSON</div>
+      <div className="selection">VÉGÉ</div>
+      <div className="selection"> &lt;660KCAL </div>
     </div>
   )
 }
