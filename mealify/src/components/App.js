9ea7834fdb93f3a3341c31744f98163a2031bf50
tony <tony.bruchon@hotmail.fr>
2024-12-06 01:22:40 +0100

    Added the ability to select between four categories of dishes

--- mealify/src/components/App.js
+++ mealify/src/components/App.js
@@ -1,9 +1,13 @@
 import '../styles/App.css';
 import Banner from "./Banner"
+import Selection from "./Selection"
 
 function App() {
   return (
-    <Banner />
+    <>
+      <Banner />
+      <Selection />
+    </>
   )
 }
 
---
+++ mealify/src/components/Selection.js
@@ -0,0 +1,14 @@
+import "../styles/Selection.css"
+
+function Selection() {
+  return (
+    <div className="selectionList">
+      <div className="selection">Pour bientôt</div>
+      <div className="selection">Pour bientôt</div>
+      <div className="selection">Pour bientôt</div>
+      <div className="selection">Pour bientôt</div>
+    </div>
+  )
+}
+
+export default Selection
