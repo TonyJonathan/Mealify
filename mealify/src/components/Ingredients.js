9adf980fbdcdd03c8dd346f8ab41f37177baa1c9
tony <tony.bruchon@hotmail.fr>
2024-12-10 21:46:21 +0100

    Added the Ingredients.js file to list all the ingredients.

---
+++ mealify/src/components/Ingredients.js
@@ -0,0 +1,22 @@
+import basil from "../assets/ingredients/basil.webp"
+import beffSteak from "../assets/ingredients/beffSteak.webp"
+import breadcrumbs from "../assets/ingredients/breadcrumbs.webp"
+import egg from "../assets/ingredients/egg.webp"
+import parmesan from "../assets/ingredients/parmesan.webp"
+import shallot from "../assets/ingredients/shallot.webp"
+import spaghetti from "../assets/ingredients/spaghetti.webp"
+import tomatoSauce from "../assets/ingredients/tomatoSauce.webp"
+
+
+const ingredients = {
+  "Pâtes (spaghetti)": spaghetti, 
+  "Parmesan (râpé)": parmesan, 
+  "Boeuf (steack haché frais)": beffSteak, 
+  "Échalote": shallot, 
+  "Sauce tomate": tomatoSauce, 
+  "Basilic (frais)": basil, 
+  "Chapelure": breadcrumbs, 
+  "Œuf": egg
+}
+
+export default ingredients
