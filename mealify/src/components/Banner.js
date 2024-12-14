d2a43d110eeb8a552835f2ba94ff00ee270b632c
tony <tony.bruchon@hotmail.fr>
2024-12-14 21:22:58 +0100

    Layout modification of the banner for screens under 600px, to improve the display and centering of elements.

--- mealify/src/components/Banner.js
+++ mealify/src/components/Banner.js
@@ -7,9 +7,9 @@ function Banner() {
     <>
       <h1 className="top-banner">Votre menu de la semaine en un clic !</h1>
       <div className="div-logo">
-        <img src={logo} alt="mealify" width="175px"></img>
+        <img src={logo} alt="mealify"></img>
         <div className="div-search-input">
-          <img src={searchLogo} alt="search logo" width="20px" height="20px" className="search-logo"></img>
+          <img src={searchLogo} alt="search logo"  className="search-logo"></img>
           <input type="text" placeholder="Rechercher..." className="search-input"></input>
         </div>
       </div>
