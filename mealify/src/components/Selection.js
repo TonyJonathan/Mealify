fa442663d16f271b0c856381cf08103235c840fd
tony <tony.bruchon@hotmail.fr>
2024-12-14 19:10:37 +0100

    Added a dropdown menu (select) with correlation between the buttons and the select.

--- mealify/src/components/Selection.js
+++ mealify/src/components/Selection.js
@@ -2,22 +2,27 @@ import "../styles/Selection.css"
 import React, { useState } from "react"
 
 function Selection() {
-  const [activeButton, setActiveButton] = useState(null);
-  const buttons = ["VIANDES 🥩", "POISSON 🐠", "VÉGÉ 🥗", "< 660KCAL 📉"]; 
+  const [activeSelection, setActiveSelection] = useState("TOUS 🍽️");
+  const buttons = ["TOUS 🍽️", "VIANDES 🥩", "POISSON 🐠", "VÉGÉ 🥗"]; 
+  const selectOptions = [<option key="0">Tous les plats</option>]; 
+  for (let i = 0; i < buttons.length; i++){
+    selectOptions.push(<option key={i + 1}>{buttons[i]}</option>)
+  }
+
   return (
     <>
     <div className="selectionList">
     {buttons.map((label, index) => (
       <button
         key={index}
-        className={`selection ${activeButton === index ? "active" : ""}`}
-        onClick={() => setActiveButton(index)}
-        onBlur={() => setActiveButton(null)}
+        className={`selectionButtons ${activeSelection === label ? "active" : ""}`}
+        onClick={() => setActiveSelection(label)}
       >
         {label}
       </button>
     ))}
-    </div>
+      </div>
+      <select value={activeSelection} onChange={(e) => setActiveSelection(e.target.value)}>{selectOptions}</select>
       <div className="separationLine"></div>
       </>
 );
