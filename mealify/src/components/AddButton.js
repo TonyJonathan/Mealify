0ce3b39431385ccaa60c6ec8d9ac1838c6f7bd40
tony <tony.bruchon@hotmail.fr>
2024-12-28 18:54:57 +0100

    Creation of AddButton compenent

---
+++ mealify/src/components/AddButton.js
@@ -0,0 +1,39 @@
+import AddDish from "./AddDish"
+
+function AddButton({ meal, addToMenu, setAddToMenu }) {
+  const mealNumber = (meal) => {
+    const dish = addToMenu.find((dish) => dish.name === meal.name)
+    return dish ? dish.quantity : 0
+  }
+
+  return (
+      <>
+          {addToMenu.find((dish) => dish.name === meal.name && dish.quantity > 0) ? 
+            <div className="addMealSelection">
+              <button
+                onClick={() => AddDish({ meal, addToMenu, setAddToMenu, servingsNumber: -1 })}
+              >
+                -
+              </button>
+              <input
+                type="text"
+                value={mealNumber(meal)}
+                disabled />
+              <button
+                onClick={() => AddDish({ meal, addToMenu, setAddToMenu, servingsNumber: 1 })}
+              >
+                +
+              </button>
+            </div> : 
+            <button
+              className="addMeal"
+              onClick={() => AddDish({ meal, addToMenu, setAddToMenu, servingsNumber: 1 })}            
+            >
+              Ajouter
+            </button>
+        }
+    </>
+  )
+}
+
+export default AddButton
--- mealify/src/components/MealSelection.js
+++ mealify/src/components/MealSelection.js
@@ -4,16 +4,13 @@ import React, { useState } from "react";
 import Modal from "./Modal";
 import "../styles/Modal.css"
 import AddDish from "./AddDish"
+import AddButton from "./AddButton"
 
 
 function MealSelection() {
   const [openMealId, setOpenMealId] = useState(null);
   const [servingsNumber, setServingsNumber] = useState(1);
   const [addToMenu, setAddToMenu] = useState([]); 
-  const mealNumber = (meal) => {
-    const dish = addToMenu.find((dish) => dish.name === meal.name)
-    return dish ? dish.quantity : 0
-  }
   
   
   return (
@@ -37,27 +34,7 @@ function MealSelection() {
               <p className="nutritionalValues">Glucides {meal.carbohydrates}</p>
               <p className="nutritionalValues">Graisses {meal.lipids}</p>
           </div>
-          {addToMenu.find((dish) => dish.name === meal.name && dish.quantity > 0) ? 
-            <div className="addMealSelection">
-              <button
-                onClick={() => AddDish({ meal, addToMenu, setAddToMenu, servingsNumber: -1 })}
-              >
-                -
-              </button>
-              <input
-                type="text"
-                value={mealNumber(meal)}
-                disabled />
-              <button
-                onClick={() => AddDish({ meal, addToMenu, setAddToMenu, servingsNumber: 1 })}
-              >
-                +
-              </button>
-            </div> : 
-            <button
-              className="addMeal"
-              onClick={() => AddDish({ meal, addToMenu, setAddToMenu, servingsNumber: 1 })}            
-            >Ajouter</button>}
+            <AddButton meal={meal} addToMenu={addToMenu} setAddToMenu={setAddToMenu} />
             
           </div>
           
