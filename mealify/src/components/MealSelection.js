5d480a5cac57d5bd432de864266eedcb034e7e79
tony <tony.bruchon@hotmail.fr>
2024-12-29 21:02:01 +0100

    Adjust layout to display dishes correctly based on selected type

--- mealify/src/components/MealSelection.js
+++ mealify/src/components/MealSelection.js
@@ -5,13 +5,11 @@ import Modal from "./Modal";
 import "../styles/Modal.css"
 import AddButton from "./AddButton"
 
-
 const mealType = {
   "VÉGÉ 🥗": "veggie", 
   "VIANDES 🥩": "meat", 
   "POISSON 🐠": "fish"
 }
-
 
 function MealSelection({ activeSelection }) {
   const [openMealId, setOpenMealId] = useState(null);
@@ -21,33 +19,30 @@ function MealSelection({ activeSelection }) {
   
   return (
       <div className="container">
-      {MealList.map((meal) => 
-        meal.type === mealType[activeSelection] || activeSelection === "TOUS 🍽️" ? (
-        <div className="mealContainer" key={meal.name}>
-          <img src={meal.cover} alt={meal.name} width="252.5px"
-            onClick={() => setOpenMealId(meal.id)}>
-          </img>
-          <div>
-            {openMealId === meal.id ? (
-              <Modal isOpen={openMealId} onClose={() => setOpenMealId(null)} meal={meal} servingsNumber={servingsNumber} setServingsNumber={setServingsNumber} addToMenu={addToMenu} setAddToMenu={setAddToMenu}  />
-            )
-              : null
-            }
+        {MealList.map((meal) => 
+          meal.type === mealType[activeSelection] || activeSelection === "TOUS 🍽️" ? (
+            <div className="mealContainer" key={meal.name}>
+              <img src={meal.cover} alt={meal.name} width="252.5px"
+                onClick={() => setOpenMealId(meal.id)}>
+              </img>
+              <div>
+                {openMealId === meal.id ? (
+                  <Modal isOpen={openMealId} onClose={() => setOpenMealId(null)} meal={meal} servingsNumber={servingsNumber} setServingsNumber={setServingsNumber} addToMenu={addToMenu} setAddToMenu={setAddToMenu}  />
+                )
+                  : null
+                }
+                </div>
+                <p className="mealTitle">{meal.name}</p>
+                <div className="nutritionalValuesContainer">
+                  <p className="nutritionalValues">Calories {meal.kcal}</p>
+                  <p className="nutritionalValues">Protéines {meal.proteins}</p>
+                  <p className="nutritionalValues">Glucides {meal.carbohydrates}</p>
+                  <p className="nutritionalValues">Graisses {meal.lipids}</p>
+              </div>
+                <AddButton meal={meal} addToMenu={addToMenu} setAddToMenu={setAddToMenu} />
             </div>
-            <p className="mealTitle">{meal.name}</p>
-            <div className="nutritionalValuesContainer">
-              <p className="nutritionalValues">Calories {meal.kcal}</p>
-              <p className="nutritionalValues">Protéines {meal.proteins}</p>
-              <p className="nutritionalValues">Glucides {meal.carbohydrates}</p>
-              <p className="nutritionalValues">Graisses {meal.lipids}</p>
-          </div>
-            <AddButton meal={meal} addToMenu={addToMenu} setAddToMenu={setAddToMenu} />
-            
-          </div>
-        ) : null
-          
+          ) : null
         )}
-
       </div>
     )
 }
