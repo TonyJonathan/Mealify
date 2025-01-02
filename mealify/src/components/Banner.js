2fef9b906261684d621d4bec05bfb36ebb0dfd14
tony <tony.bruchon@hotmail.fr>
2025-01-02 20:46:33 +0100

    Add icon to open 'menu' modal on click

--- mealify/src/components/Banner.js
+++ mealify/src/components/Banner.js
@@ -1,8 +1,10 @@
 import "../styles/Banner.css"
 import logo from "../assets/logo.svg"
 import searchLogo from "../assets/searchLogo.png"
+import cuterly from "../assets/icons/cuterly.svg"
 
-function Banner() {
+function Banner({ searchValue, setSearchValue }) {
+  
   return (
     <>
       <h1 className="top-banner">Votre menu de la semaine en un clic !</h1>
@@ -10,8 +12,16 @@ function Banner() {
         <img src={logo} alt="mealify"></img>
         <div className="div-search-input">
           <img src={searchLogo} alt="search logo"  className="search-logo"></img>
-          <input type="text" placeholder="Rechercher..." className="search-input"></input>
+          <input
+            type="text"
+            placeholder="Rechercher..."
+            className="search-input"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          >
+          </input>
         </div>
+        <img src={cuterly} alt="cuterly" className="cuterly" />
       </div>
       <div className="line"></div>
     </>
