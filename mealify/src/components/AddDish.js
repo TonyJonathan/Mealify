35db2a6651e1fd4d561d7ce83930d661b961941d
tony <tony.bruchon@hotmail.fr>
2025-01-08 23:25:23 +0100

    Fix AddDish: remove dish if its quantity is less than 1

--- mealify/src/components/AddDish.js
+++ mealify/src/components/AddDish.js
@@ -2,8 +2,12 @@ function AddDish({ meal, addToMenu, setAddToMenu, servingsNumber }) {
   
   const currentDishAdded = addToMenu.find((dish) => dish.name === meal.name); 
   if (currentDishAdded) {
-    const menuFilteredCurrentDish = addToMenu.filter((dish) => dish.name !== meal.name)
-    setAddToMenu([...menuFilteredCurrentDish, { name: meal.name, quantity: Number(currentDishAdded.quantity) + Number(servingsNumber) }])
+    const updatedMenu = addToMenu.map((dish) =>
+      dish.name === meal.name
+        ? { ...dish, quantity: Number(currentDishAdded.quantity) + Number(servingsNumber) }
+        : dish
+    );
+    setAddToMenu(updatedMenu.filter((dish) => dish.quantity > 0));
   } else {
     setAddToMenu([...addToMenu, { name: meal.name, quantity: servingsNumber }]); 
   }
