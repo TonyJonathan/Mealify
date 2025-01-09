0927a95b743c9690d2d26e206206f7632e9d8213
tony <tony.bruchon@hotmail.fr>
2025-01-09 23:59:51 +0100

    Create Cart.css

--- mealify/src/components/Cart.jsx
+++ mealify/src/components/Cart.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function Cart({ isOpen, onClose, addToMenu, setAddToMenu }) {
   if (!isOpen) return null 
-  console.log(addToMenu)
+  console.log(addToMenu.length)
   return (
     <>
       <div className="cart-overlay" onClick={onClose}>
@@ -12,6 +12,12 @@ function Cart({ isOpen, onClose, addToMenu, setAddToMenu }) {
           <button className="modal-close" onClick={onClose}>
             ×
           </button>
+          {addToMenu.length === 0 ? (
+            <div className="empty-cart">
+              <p>Votre panier est vide</p>
+              <button>Retour à la sélection</button>
+            </div>
+          ): null}
           
         </div>
       </div>
