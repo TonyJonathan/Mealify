8b9d1da66240c6d984f4e8e05914f878e6a80e41
tony <tony.bruchon@hotmail.fr>
2025-01-31 16:20:35 +0100

    Modified RecipeSection to align with changes in the new structure

--- src/components/RecipeSection.jsx
+++ src/components/RecipeSection.jsx
@@ -6,18 +6,18 @@ function RecipeSection({ meal }) {
     <>
       <div className="recipe-section">
           <p className="title-sections">Recette</p>
-          {meal.recipe.map((step, index) => 
+          {meal.recipe.map((recipe, index) => 
             <div className="step" key={index}>
               <div className="ingredients-list">
-              {Array.isArray(step[1]) ? (
-                step[1].map((ingredient) => (
+              {Array.isArray(recipe.ingredient) ? (
+                recipe.ingredient.map((ingredient) => (
                 <img src={getImagePath(ingredient)} alt={ingredient} key={ingredient} className="ingredients-picture" />
                 ))
 )               : null}
               </div>
               <div>
                 <p className="step-number">Étape {index + 1}</p>
-                <p className="text-sections">{step[0]}</p>
+                <p className="text-sections">{recipe.step}</p>
               </div>
             </div>
           )}
