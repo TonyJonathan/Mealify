ce06cb0da0a64d17394e9c8cf61acffb42d3ddc1
tony <tony.bruchon@hotmail.fr>
2025-02-10 20:08:37 +0100

    Creation of MenuContext composent

---
+++ src/components/MenuContext.jsx
@@ -0,0 +1,23 @@
+// src/MenuContext.js
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const MenuContext = createContext();
+
+export const useMenu = () => {
+  return useContext(MenuContext);
+};
+
+export const MenuProvider = ({ children }) => {
+  const savedMenu = localStorage.getItem('menu');
+  const [addToMenu, setAddToMenu] = useState(() => (savedMenu ? JSON.parse(savedMenu) : []));
+  
+  useEffect(() => {
+    localStorage.setItem('menu', JSON.stringify(addToMenu));
+  }, [addToMenu]);
+  
+  return (
+    <MenuContext.Provider value={{ addToMenu, setAddToMenu }}>
+      {children}
+    </MenuContext.Provider>
+  );
+};
