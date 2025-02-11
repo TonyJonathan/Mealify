25e61da9a8c9a366382da0b00c641bebff04e624
tony <tony.bruchon@hotmail.fr>
2025-02-11 20:44:08 +0100

    Refactored App to use MenuContext for state management

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,29 @@ import '../styles/App.css'
 import Banner from "./Banner"
 import Selection from "./Selection"
 import MealSelection from './MealSelection'
-import { useState, useEffect } from "react"
-import React from "react"
+import React, { useState } from "react"
+import { MenuProvider, useMenu } from "./MenuContext"
 
-function App() {
+function AppContent() {
+  const { addToMenu, setAddToMenu } = useMenu(); // Récupération des valeurs depuis le contexte
   const [activeSelection, setActiveSelection] = useState("TOUS 🍽️");
-  const [searchValue, setSearchValue] = useState('')
-  const savedMenu = localStorage.getItem('menu')
-  const [addToMenu, setAddToMenu] = useState(() => (savedMenu ? JSON.parse(savedMenu) : []))
+  const [searchValue, setSearchValue] = useState('');
 
-  useEffect(() => {localStorage.setItem('menu', JSON.stringify(addToMenu))}, [addToMenu])
-  
   return (
     <>
       <Banner searchValue={searchValue} setSearchValue={setSearchValue} addToMenu={addToMenu} setAddToMenu={setAddToMenu} />
       <Selection activeSelection={activeSelection} setActiveSelection={setActiveSelection} />
       <MealSelection activeSelection={activeSelection} searchValue={searchValue} addToMenu={addToMenu} setAddToMenu={setAddToMenu} />
     </>
-  )
+  );
+}
+
+function App() {
+  return (
+    <MenuProvider>
+      <AppContent />
+    </MenuProvider>
+  );
 }
 
 export default App;
