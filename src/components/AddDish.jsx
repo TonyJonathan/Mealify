7376d0dc99c189c566be106f5cb609cefb8f4d8f
tony <tony.bruchon@hotmail.fr>
2025-02-20 23:40:42 +0100

    Create useAddDish hook to manage adding dishes to the menu

--- src/components/AddDish.jsx
+++ src/components/AddDish.jsx
@@ -1,16 +1,26 @@
-function AddDish({ meal, addToMenu, setAddToMenu, servingsNumber }) {
-  
-  const currentDishAdded = addToMenu.find((dish) => dish.name === meal.name)
-  if (currentDishAdded) {
-    const updatedMenu = addToMenu.map((dish) =>
-      dish.name === meal.name
-        ? { ...dish, quantity: Number(currentDishAdded.quantity) + Number(servingsNumber) }
-        : dish
-    )
-    setAddToMenu(updatedMenu.filter((dish) => dish.quantity > 0))
-  } else {
-    setAddToMenu([...addToMenu, { name: meal.name, quantity: servingsNumber }])
-  }
+import { useCallback } from "react";
+import { useMenu } from "./MenuContext";
+
+function useAddDish() {
+  const { addToMenu, setAddToMenu } = useMenu();
+
+  const addDish = useCallback((meal, servingsNumber) => {
+    const currentDishAdded = addToMenu.find((dish) => dish.name === meal.name);
+    console.log(servingsNumber)
+    if (currentDishAdded) {
+      
+      const updatedMenu = addToMenu.map((dish) =>
+        dish.name === meal.name
+          ? { ...dish, quantity: Number(currentDishAdded.quantity) + Number(servingsNumber) }
+          : dish
+      );
+      setAddToMenu(updatedMenu.filter((dish) => dish.quantity > 0));
+    } else {
+      setAddToMenu([...addToMenu, { name: meal.name, quantity: servingsNumber }]);
+    }
+  }, [addToMenu, setAddToMenu]);
+
+  return addDish;
 }
 
-export default AddDish 
+export default useAddDish;
