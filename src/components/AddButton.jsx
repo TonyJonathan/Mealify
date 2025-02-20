7389fb9781ddc7c0a7dc2aed5a16621229fe8f50
tony <tony.bruchon@hotmail.fr>
2025-02-20 23:42:11 +0100

    Update AddButton.jsx to use useAddDish hook for handling dish quantity

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,43 +1,30 @@
-import AddDish from "./AddDish"
-import React from "react"
-import { useMenu } from "./MenuContext"  
+import React from "react";
+import useAddDish from "./addDish";
+import { useMenu } from "./MenuContext";
 
-
-function AddButton({ meal }) {  
-  const { addToMenu, setAddToMenu } = useMenu()
+function AddButton({ meal }) {
+  const addDish = useAddDish();
+  const { addToMenu } = useMenu();
   const mealNumber = (meal) => {
-    const dish = addToMenu.find((dish) => dish.name === meal.name)
-    return dish ? dish.quantity : 0
-  }
+    const dish = addToMenu.find((dish) => dish.name === meal.name);
+    return dish ? dish.quantity : 0;
+  };
 
   return (
-      <>
-          {addToMenu.find((dish) => dish.name === meal.name && dish.quantity > 0) ? 
-            <div className="addMealSelection">
-              <button
-                onClick={() => AddDish({ meal, addToMenu, setAddToMenu, servingsNumber: -1 })}
-              >
-                -
-              </button>
-              <input
-                type="text"
-                value={mealNumber(meal)}
-                disabled />
-              <button
-                onClick={() => AddDish({ meal, addToMenu, setAddToMenu, servingsNumber: 1 })}
-              >
-                +
-              </button>
-            </div> : 
-            <button
-              className="addMeal"
-              onClick={() => AddDish({ meal, addToMenu, setAddToMenu, servingsNumber: 1 })}            
-            >
-              Ajouter
-            </button>
-        }
+    <>
+      {addToMenu.find((dish) => dish.name === meal.name && dish.quantity > 0) ? (
+        <div className="addMealSelection">
+          <button onClick={() => addDish(meal, -1)}>-</button>
+          <input type="text" value={mealNumber(meal)} disabled />
+          <button onClick={() => addDish(meal, 1)}>+</button>
+        </div>
+      ) : (
+        <button className="addMeal" onClick={() => addDish(meal, 1)}>
+          Ajouter
+        </button>
+      )}
     </>
-  )
+  );
 }
 
-export default AddButton
+export default AddButton;
